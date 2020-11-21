85d419253868dea92e8f9ee76efd4e292fe697fd
Gina <e.kurmeneva@gmail.com>
2020-11-21 13:20:05 +0300

    Get the JSON data

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,19 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const Context = React.createContext();
 
 function ContextProvider({children}) {
     const [allPokemons, setAllPokemons] = useState([]);
+    const url = "http://localhost:3004/pokemons";
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setAllPokemons(data))
+    }, []);
+
+    console.log(allPokemons);
+
     return (
         <Context.Provider value={{allPokemons}}>
             {children}
