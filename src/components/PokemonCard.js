dcecd2393f19c0e752f975611fe1dae6fa719842
Gina <e.kurmeneva@gmail.com>
2020-11-21 21:02:33 +0300

    Show border if the card is hovered

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
     Col,
     Card,
@@ -10,9 +10,17 @@ import {
 
 function PokemonCard({pokemon}) {
     let img = pokemon.id <= 720 ? (process.env.PUBLIC_URL + `pokemons/${pokemon.id}.png`) : (process.env.PUBLIC_URL + "pokemons/confused_travolta.png");
+    const [hovered, setHovered] = useState(false);
+    const hoverEffect = hovered && "primary";
+
     return (
         <Col xs="12" sm="6" md="4" lg="3">
-            <Card className="mt-4">
+            <Card className="mt-4"
+                  outline
+                  color={hoverEffect}
+                  onMouseEnter={() => setHovered(true)}
+                  onMouseLeave={() => setHovered(false)}
+            >
                 <CardHeader tag="h5" className="text-capitalize text-center">
                     {pokemon.name}
                 </CardHeader>
