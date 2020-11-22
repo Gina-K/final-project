f6f7174a9f5d0dbb5b072b37a989c7ff9ef76610
Gina <e.kurmeneva@gmail.com>
2020-11-22 16:46:06 +0300

    Send updated (caught) pokemon to the json-server

--- src/Context.js
+++ src/Context.js
@@ -13,14 +13,30 @@ function ContextProvider({children}) {
     }, []);
 
     function catchPokemon(id) {
+        let caughtPokemon;
         const today = new Date().toLocaleDateString("en-GB", {day: "2-digit", month: "long", year: "numeric"});
         const updatedArray = allPokemons.map(pokemon => {
             if (pokemon.id === id) {
-                return {...pokemon, isCaught: true, captureDate: today};
+                caughtPokemon = {...pokemon, isCaught: true, captureDate: today};
+                return caughtPokemon;
             }
             return pokemon;
         });
         setAllPokemons(updatedArray);
+        putUpdate(caughtPokemon);
+    }
+
+    function putUpdate(pokemon) {
+        let updateUrl = `${url}/${pokemon.id}`;
+        fetch(updateUrl, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(pokemon),
+        })
+            .then(response => response.json())
+            .then(data => console.log(data));
     }
 
     return (
